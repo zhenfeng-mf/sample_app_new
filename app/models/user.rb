1f9f55fa2e9cfeab7b7a8913ce5e1a061623dcb4
zhenfeng-mf <liao.zhenfeng@moneyforward.co.jp>
2026-07-09 08:44:29 +0000

    6.3 Added password to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,36 @@ class User < ApplicationRecord
   # end
   before_save { self.email = email.downcase }
   # validates(attribute_to_be_validate, {validation options})
-  validates(:name,
-            {
-              presence: true,
-              length: { maximum: 50 }
-            })
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+  validates(
+    :name,
+    {
+      presence: true,
+      length: { maximum: 50 }
+    }
+  )
 
-  validates(:email,
-            {
-              presence: true,
-              length: { maximum: 255 },
-              format: { with: VALID_EMAIL_REGEX },
-              uniqueness: { case_sensitive: false }
-            })
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+  validates(
+    :email,
+    {
+      presence: true,
+      length: { maximum: 255 },
+      format: { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
+    }
+  )
+
+  # has_secure_password can
+  # 1. Store hashed pswd into password_digest attribute
+  # 2. Access 2 virtual attributes password & password_confirmation;
+  #    presence and matching validations are auto added.
+  # 3. authenticate() method. Returns the user object if pswd is correct, false otherwise
+  has_secure_password
+  validates(
+    :password,
+    {
+      presence: true,
+      length: { minimum: 8 }
+    }
+  )
 end
