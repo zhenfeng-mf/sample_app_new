a9feeb7679faa002fcc0c25d07b184864d9c0d3f
zhenfeng-mf <liao.zhenfeng@moneyforward.co.jp>
2026-07-09 07:29:04 +0000

    6.1 - 6.2 Created User model with name and email fields. Added :name validation for presence and length. Added :email validation for presence, length, format, and uniqueness. Added index to email column in database.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
